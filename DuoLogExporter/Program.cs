2e23553e9ce1e569e994fe800fb002422b923b6a
Stephen Zura <swzura@gmail.com>
2024-04-10 13:31:26 -0400

    Initial commit

---
+++ DuoLogExporter/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DuoLogExporter;
+
+
+internal class Program
+{
+    //set us up to read from config and to make rest calls
+    private static IConfigurationRoot? _config;
+    private static readonly HttpClient _http = new();
+    
+    private static readonly string? DuoEndPoint = Environment.GetEnvironmentVariable("duoEndPoint");
+    private static readonly string? DuoClientKey = Environment.GetEnvironmentVariable("duoClientKey");
+    private static readonly string? DuoClientSecret = Environment.GetEnvironmentVariable("duoClientSecret");
+    
+    //Default the output path to the same directory as the executable
+    private static string? outputPath = Directory.GetCurrentDirectory();
+    
+
+// string myConfigValue = configuration["yourKey"];
+    private static void Main(string[] args)
+    {
+        // Add the following code to your Main method:
+        _config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        outputPath = string.IsNullOrWhiteSpace(_config.GetSection("OutputPath").Value) ? Directory.GetCurrentDirectory() : _config.GetSection("OutputPath").Value; 
+        
+        var program = new Program();
+        Console.WriteLine("Starting Up, Fetching Credentials....");
+        if (DuoEndPoint == null || DuoClientKey == null || DuoClientSecret == null)
+        {
+            Console.WriteLine("Missing required credentials. Exiting...");
+            Environment.Exit(1);
+        }
+        
+        Console.WriteLine($"Outputting to: {outputPath}");
+        
+        Console.WriteLine(program.HttpCall());
+    }
+
+    public string HttpCall()
+    {
+        return "success";
+    }
+}
---
+++ duo_api_csharp/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b15c44a4-74d6-45b7-8a30-a313c2818083")]
+
+// Allow tests to access internal methods for easier testing
+[assembly: InternalsVisibleTo("DuoApiTest")]
